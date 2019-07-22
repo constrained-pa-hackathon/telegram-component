dfff46af89659dbd89525da9ffa8c60aa024b97c
Timor <test.test.com>
2019-07-22 22:09:58 +0300

    added try catch and text command support

--- main.py
+++ main.py
@@ -14,25 +14,25 @@ def wav_to_ogg(file_name):
     wav_audio.export(file_name+'.ogg', format='ogg')
 
 def send_voice_to_stt(audio_file):
-    url = "http://rav2.co.il:4444/speech_to_text"
-    audio_file.download("check.ogg")
-    file_name = "check"
+    url = 'http://rav2.co.il:4444/speech_to_text'
+    audio_file.download('check.ogg')
+    file_name = 'check'
     ogg_to_wav(file_name)
-    data = open(file_name+".wav", "rb")
+    data = open(file_name+'.wav', 'rb')
     payload = {'sentence-audio': data}
     response = requests.post(url=url, files=payload)
     print(response.text)
-    return json.loads(response.text)["output_text"]
+    return json.loads(response.text)['output_text']
 
 def send_text_to_parse_sentence(sentence):
-    url = "http://rav2.co.il:5000/parse_sentence"
+    url = 'http://rav2.co.il:5000/parse_sentence'
     payload = {'sentence': sentence}
     response = requests.post(url=url, data=payload)
     print(response.text)
     return json.dumps(json.loads(response.text), indent=4, sort_keys=True)
 
 def send_command_to_simulation(command):
-    url = "http://rav2.co.il:3000/response"
+    url = 'http://rav2.co.il:3000/response'
     command = json.loads(command)
     response = requests.post(url=url, json=command);
     print(response.text)
@@ -40,11 +40,44 @@ def send_command_to_simulation(command):
 
 def send_response_to_synthesizer(response):
     text = json.loads(response)['text']
-    url ="http://rav2.co.il:3000/synth"
-    response = requests.post(url=url, params={"text" : text})
+    url ='http://rav2.co.il:3000/synth'
+    response = requests.post(url=url, params={'text' : text})
     with open('synth.wav', 'wb') as f:
         f.write(response.content)
     wav_to_ogg('synth')
+
+def handle_speech_to_text(bot, update):
+    voice = bot.get_file(update.message.voice.file_id)
+    try:
+        text = send_voice_to_stt(voice)
+    except:
+        update.message.reply_text('Exception: Conversion from .ogg to .wav file')
+    return text
+
+def handle_main_proces(bot, update, text):
+    print(type(text))
+    print(text)
+    try:
+        text = send_text_to_parse_sentence(text)
+    except:
+        update.message.reply_text('Exception: Intent Command')
+        return
+    update.message.reply_text(text)
+
+    try:
+        text = send_command_to_simulation(text)
+    except:
+        update.message.reply_text('Exception: Response')
+        return
+    update.message.reply_text(text)
+    try:
+        send_response_to_synthesizer(text)
+        synth = open('synth.ogg', "rb")
+        chat_id = update.message.chat.id
+        bot.send_voice(chat_id=chat_id, voice=synth)
+    except:
+        update.message.reply_text('Exception: Synth')
+        return
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -64,24 +97,25 @@ def help(bot, update):
     """Send a message when the command /help is issued."""
     update.message.reply_text('Help!')
 
-def echo(bot, update):
+def voice_command(bot, update):
     """Echo the user message."""
-    new_file = bot.get_file(update.message.voice.file_id)
-    text = send_voice_to_stt(new_file)
+    try:
+        text = handle_speech_to_text(bot, update)
+    except:
+        update.message.reply_text('Exception: Speech To Text')
+        return
+
     if text == " ":
         update.message.reply_text("Error: Can't detect speech")
         return
     else:
         update.message.reply_text(text)
-    text = send_text_to_parse_sentence(text)
-    print(text)
-    update.message.reply_text(text)
-    text = send_command_to_simulation(text)
-    update.message.reply_text(text)
-    send_response_to_synthesizer(text)
-    synth = open("synth.ogg", "rb")
-    chat_id = update.message.chat.id
-    bot.send_voice(chat_id=chat_id, voice=synth)
+
+    handle_main_proces(bot, update, text)
+
+def text_command(bot, update):
+    handle_main_proces(bot, update, update.message.text)
+    #handle_main_proces(bot, update, "set frequency to 555.33")
 
 def error(bot, update, error):
     """Log Errors caused by Updates."""
@@ -100,7 +134,8 @@ def main():
     dp.add_handler(CommandHandler("help", help))
 
     # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.voice, echo))
+    dp.add_handler(MessageHandler(Filters.voice, voice_command))
+    dp.add_handler(MessageHandler(Filters.text, text_command))
 
     # log all errors
     dp.add_error_handler(error)
